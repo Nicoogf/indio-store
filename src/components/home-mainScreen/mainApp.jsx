6dd438f237865cfe12dae86a2e8a617042121818
Nicoogf <nicolasgfalabella@gmail.com>
2023-08-12 14:14:33 -0300

    Avance : Se modifica la estructura del grid-store

--- src/components/home-mainScreen/mainApp.jsx
+++ src/components/home-mainScreen/mainApp.jsx
@@ -11,7 +11,7 @@ function MainScreenApp() {
 
         <h2>Indio</h2>
         <h3>Solari</h3>
-        <h4>Encuentra todos los <span>productos , novedades y sus proximas fechas</span> para vivir una experiencia unica</h4>
+        <h4>Sumérgete en la esencia de la banda a través de nuestro documental. <span>Experimenta un poco de lo que fue unas de las noches mas recordadas del rock argentino como lo fue en su visita en Tandil.</span> Revive la emoción y la energía de su última actuación</h4>
    
         <a href="https://youtu.be/L-_vywGs8-M" target='_blank'> Ver Tsunami</a>
         </div>
--- src/screens/store/store.jsx
+++ src/screens/store/store.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import title_store from "../../assets/store.png"
-import product_1 from "../../assets/producto_1.png"
-import product_2 from "../../assets/producto_2.png"
-import product_5 from "../../assets/producto_5.png"
-import product_13 from "../../assets/producto_13.png"
-import product_7 from "../../assets/producto_7.png" ;
-
-import product_12 from "../../assets/producto_12.jpg"
-
-
 import "./store.css";
+
+import recitales_title from "../../assets/recitales.png"
+
+
 
 function Store() {
 
@@ -18,110 +12,45 @@ function Store() {
 
   return (
     
-    <div className='store-container' id="store">
+    <div className='store-main' id="store">
 
-        <section className='product-container'>
+        <div className='store-container'>
 
-            <img src={ title_store } alt="" className='img-store'/>
+        <img src={recitales_title} alt="" className='img-recitales'/>
 
-            <div className='product-countent'>
+        <div className='products'>
 
-                <article className='product'>
+            <div className='product'>
 
-                <h6>Album</h6>
+            </div> 
 
-                <img src={ product_2 } alt="" className='img-producto'/>
-
-                <div className='info-producto'>
-                    <h4>Album</h4>
-                    <h6>Ceremonia en la Tormenta</h6>
-                    <p>$15.000</p>
-                </div>
+            <div className='product'>
                 
-
-                </article>
-
-                <article className='product'>
-
-                <h6>Remera</h6>
-
-                <img src={ product_5} alt="" className='img-producto'/>
-
-                <div className='info-producto'>
-                    <h4>Remera</h4>
-                    <h6>Edicion PorcoRex</h6>
-                    <p>$10.000</p>
-                </div>
-                
-
-                </article>
-
-
-                <article className='product'>
-
-                    <h6>Remera</h6>
-
-                    <img src={product_13 } alt="" className='img-producto'/>
-
-                    <div className='info-producto'>
-                        <h4>Libro</h4>
-                        <h6>Delito Americano</h6>
-                        <p>$1500.000</p>
-                    </div>
-
-
-                </article>
-
-                <article className='product'>
-
-                    <h6>Remera</h6>
-
-                    <img src={product_1 } alt="" className='img-producto'/>
-
-                    <div className='info-producto'>
-                        <h4>Album</h4>
-                        <h6>El Tesoro de los inocentes (Bingo-Fuel)</h6>
-                        <p>$1500.000</p>
-                    </div>
-
-
-                </article>
-
-                <article className='product'>
-
-                <h6>Remera</h6>
-
-                <img src={product_12 } alt="" className='img-producto'/>
-
-                <div className='info-producto'>
-                    <h4>Libro</h4>
-                    <h6>La vida es una mision secreta</h6>
-                    <p>$15000.00</p>
-                </div>
-
-
-                </article>
-
-                <article className='product'>
-
-                <h6>Remera</h6>
-
-                <img src={product_7 } alt="" className='img-producto'/>
-
-                <div className='info-producto'>
-                    <h4>Remera</h4>
-                    <h6>Edicion cantante Timido</h6>
-                    <p>$7800.00</p>
-                </div>
-
-
-                </article>
-              
-              
-
             </div>
 
-        </section>      
+            <div className='product'>
+                
+            </div>
+
+            <div className='product'>
+                
+            </div>
+
+            <div className='product'>
+                
+            </div>
+
+            <div className='product'>
+                
+            </div>
+
+        </div>
+
+
+        </div>
+
+        
+      
         
     </div>
   )
